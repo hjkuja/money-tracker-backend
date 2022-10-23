6e9bc24266f764e7b715c774fcb7f9b1780593c4
Hannu Kujanpää <hannuku.hk@gmail.com>
2022-10-23 15:32:27 +0300

    Add databse context and account entity

---
+++ src/MoneyTracker.Api/Context/Entities/Account.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyTracker.Api.Context.Entities
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public Account(string username)
+        {
+            Username = username;
+        }
+    }
+}
---
+++ src/MoneyTracker.Api/Context/MoneyTrackerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyTracker.Api.Context.Entities;
+
+namespace MoneyTracker.Api.Context
+{
+    public class MoneyTrackerContext : DbContext
+    {
+        public MoneyTrackerContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Account> Accounts => Set<Account>();
+    }
+}
