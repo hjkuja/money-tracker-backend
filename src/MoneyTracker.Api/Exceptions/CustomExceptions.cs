4e2ff1faefa7ef2f35ef7de2e605e153e804d5dd
Hannu Kujanpää <61244601+hjkuja@users.noreply.github.com>
2022-10-23 15:32:01 +0300

    Add database connection and custom exceptions

---
+++ src/MoneyTracker.Api/Exceptions/CustomExceptions.cs
@@ -0,0 +1,19 @@
+﻿namespace MoneyTracker.Api.Exceptions
+{
+    public class CustomExceptions
+    {
+        /// <summary>
+        /// Custom error for missing configurations.
+        /// </summary>
+        [Serializable]
+        public class MissingConfigurationException : Exception
+        {
+            public MissingConfigurationException() { }
+            public MissingConfigurationException(string message) : base(message) { }
+            public MissingConfigurationException(string message, Exception inner) : base(message, inner) { }
+            protected MissingConfigurationException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        }
+    }
+}
