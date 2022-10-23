07385e2cad22df349f248ac7c442a38205e0b60b
Hannu Kujanpää <hannuku.hk@gmail.com>
2022-10-23 15:32:01 +0300

    Add database connection and custom exceptions

--- src/MoneyTracker.Api/Program.cs
+++ src/MoneyTracker.Api/Program.cs
@@ -1,17 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using MoneyTracker.Api.Context;
+using MoneyTracker.Api.Exceptions;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
+var connectionString = builder.Configuration["ConnectionString"];
+
+if (connectionString == null)
+{
+    throw new CustomExceptions.MissingConfigurationException("Database connection string is missing. Please add ConnectionString to correct appsettings.json");
+}
+
+// Add database context
+builder.Services.AddDbContext<MoneyTrackerContext>(
+    options => options
+        .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => {
+builder.Services.AddSwaggerGen(options =>
+{
     options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
     {
         Version = "0.0.1",
         Title = "Money Tracker API",
         Description = "This is an API for Money Tracker."
     });
+
+    var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
 });
 
 
@@ -21,6 +47,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseStaticFiles();
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
